bfe6842e8fc34ce9631ab8058cd7dda9a95b9175
User <prrvchr@gmail.com>
2022-03-10 01:16:24 +0100

    Fix Mac OSX database creation

--- lib/uno/dbtool/dbtool.py
+++ lib/uno/dbtool/dbtool.py
@@ -465,6 +465,7 @@ def getRowValue(row, dbtype, index=1, default=None):
     return value
 
 def createStaticTable(ctx, statement, tables, readonly=False):
+    print("dbtools.createStaticTable() %s" % (tables,))
     for table in tables:
         query = getSqlQuery(ctx, 'createTable' + table)
         statement.executeUpdate(query)
