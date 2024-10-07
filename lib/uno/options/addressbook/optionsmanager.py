d98a4a1009d887b5c7205440eb9909a070d020e6
prrvchr <prrvchr@gmail.com>
2024-10-07 23:57:03 +0200

    new version 1.1.6

--- lib/uno/options/addressbook/optionsmanager.py
+++ lib/uno/options/addressbook/optionsmanager.py
@@ -50,7 +50,7 @@ class OptionsManager():
         self._logger = logger
         self._model = OptionsModel(ctx)
         self._view = OptionsView(window, OptionsManager._restart, *self._model.getViewData())
-        self._logmanager = LogManager(self._ctx, window.Peer, 'requirements.txt', g_defaultlog, g_synclog)
+        self._logmanager = LogManager(self._ctx, window, 'requirements.txt', g_defaultlog, g_synclog)
         self._logger.logprb(INFO, 'OptionsManager', '__init__()', 201)
 
     _restart = False
